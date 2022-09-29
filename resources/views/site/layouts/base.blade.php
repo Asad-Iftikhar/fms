56c63d737d73a0145beeac4ade78f20e10fa9b40
vagrant <vagrant@homestead>
2022-09-29 15:21:30 +0000

    User Site templating

--- resources/views/site/layouts/base.blade.php
+++ resources/views/site/layouts/base.blade.php
@@ -10,14 +10,102 @@
     <link rel="stylesheet" href="{{ asset("/assets/vendors/bootstrap-icons/bootstrap-icons.css") }}">
     <link rel="stylesheet" href="{{ asset("/assets/css/app.css") }}">
     {{--    <link rel="stylesheet" href="{{ asset("/assets/css/pages/auth.css") }}">--}}
-    <link rel="stylesheet" href="{{ asset("/assets/css/pages/custom_auth.css") }}">
+    <link rel="stylesheet" href="{{ asset("assets/css/site.css") }}">
 
 </head>
 <body>
-<div id="auth">
+    <div id="app">
+        <div id="main" class='layout-navbar'>
+            <header class='mb-3'>
+                <nav class="navbar navbar-expand navbar-light ">
+                    <div class="container-fluid">
+                        <a href="#"><img src="{{ asset('assets/images/logo/nxblogo.svg') }}" alt="Logo" srcset=""></a>
 
-    @yield('content')
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li class="nav-item dropdown me-3">
+                                    <a class="nav-link active dropdown-toggle" href="#" data-bs-toggle="dropdown"
+                                       aria-expanded="false">
+                                        <i class='bi bi-bell bi-sub fs-4 text-gray-600'></i>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                                        <li>
+                                            <h6 class="dropdown-header">Notifications</h6>
+                                        </li>
+                                        <li><a class="dropdown-item">No notification available</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <div class="dropdown">
+                                <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div class="user-menu d-flex">
+                                        <div class="user-name text-end me-3">
+                                            <h6 class="mb-0 text-gray-600">{{Auth::user()->username}}</h6>
+                                            @if(Auth::user()->can('admin'))
+                                                <p class="mb-0 text-sm text-gray-600">Administrator</p>
+                                            @endif
+                                        </div>
+                                        <div class="user-img d-flex align-items-center">
+                                            <div class="avatar avatar-md">
+                                                <img src="assets/images/faces/1.jpg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="icon-mid bi bi-person me-2"></i>
+                                            My Profile
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="icon-mid bi bi-gear me-2"></i>
+                                            Settings
+                                        </a>
+                                    </li>
+                                    @if(Auth::user()->can('admin'))
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="icon-mid bi bi-wallet me-2"></i>
+                                            Admin Panel
+                                        </a>
+                                    </li>
+                                    @endif
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="{{url('account/logout')}}">
+                                            <i class="icon-mid bi bi-box-arrow-left me-2"></i>
+                                            Logout
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <div id="main-content">
+                @yield('content')
+                <footer>
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2022 &copy; FMS</p>
+                        </div>
+                        <div class="float-end">
+                            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                                by <a href="https://ahmadsaugi.com">NextBridge</a></p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
 
-</div>
+
+    <script src="{{ asset("assets/js/bootstrap.bundle.min.js") }}"></script>
 </body>
 </html>
