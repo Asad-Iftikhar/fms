ef31e23e62cd024aad41fd59e0836b1b1d8500e0
vagrant <vagrant@homestead>
2022-09-29 09:05:05 +0000

    middleware update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,21 +1,36 @@
 <?php
-
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
-class Authenticate extends Middleware
-{
+class Authenticate {
+
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     * @return mixed
      */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
+    public function handle($request, Closure $next, $guard = null) {
+        if ( Auth::guard( $guard )->guest() ) {
+            if ( $request->ajax() || $request->wantsJson() ) {
+                return response( 'Unauthorized.', 401 );
+            } else {
+                // Store the current uri in the session
+                if ( $request->is( '/' ) ) {
+                    $request->session()->put( 'url.intended', 'account' );
+                } else {
+                    $request->session()->put( 'url.intended', $request->url() );
+                }
+                // Redirect to the login page
+                return redirect()->guest( 'account/login' );
+            }
         }
+        return $next( $request );
     }
+
+
 }
