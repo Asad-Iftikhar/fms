866f282ef5e91383fd581d5b144b67c8099bf057
vagrant <vagrant@homestead>
2022-10-03 13:28:15 +0000

    layout fixes

--- resources/views/site/layouts/base.blade.php
+++ resources/views/site/layouts/base.blade.php
@@ -90,18 +90,19 @@
             </header>
             <div id="main-content">
                 @yield('content')
-                <footer>
-                    <div class="footer clearfix mb-0 text-muted">
-                        <div class="float-start">
-                            <p>2022 &copy; FMS</p>
-                        </div>
-                        <div class="float-end">
-                            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                                by <a href="{{ url('') }}">NextBridge</a></p>
-                        </div>
-                    </div>
-                </footer>
+
             </div>
+            <footer style="padding: 2rem">
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2022 &copy; FMS</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                            by <a href="{{ url('') }}">NextBridge</a></p>
+                    </div>
+                </div>
+            </footer>
         </div>
     </div>
 
