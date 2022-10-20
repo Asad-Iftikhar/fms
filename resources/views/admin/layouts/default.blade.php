034d381c42259ef2fd7e0b7d1e2276d047fd6e65
Asad Iftikhar <asad-nxb>
2022-10-20 12:11:04 +0500

    minor css issues fixed

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="{{ asset("assets/vendors/simple-datatables/style.css") }}">
         <link rel="stylesheet" href="{{ asset("/assets/vendors/perfect-scrollbar/perfect-scrollbar.css") }}">
         <link rel="stylesheet" href="{{ asset("/assets/vendors/bootstrap-icons/bootstrap-icons.css") }}">
-        <link rel="stylesheet" href="{{ asset("/assets/css/custom_app.css") }}">
+        <link rel="stylesheet" href="{{ asset("/assets/css/admin.css") }}">
     @show
 </head>
 <body>
