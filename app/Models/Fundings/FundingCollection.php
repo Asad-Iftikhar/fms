230c920eda80807e780ce3f35cad60c641ef62aa
SaadaanNXB <vagrant@homestead>
2022-11-16 14:29:34 +0000

    83-notification pusher setup

--- app/Models/Fundings/FundingCollection.php
+++ app/Models/Fundings/FundingCollection.php
@@ -20,6 +20,7 @@ namespace App\Models\Fundings;
 use App\Models\Base;
 use App\Models\Fundings\FundingCollectionMessage;
 use App\Models\Events\Event;
+use App\Models\Notifications\Notification;
 use App\Models\Users\User;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
